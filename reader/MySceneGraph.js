770ed6e52dd37df9bdbde18887ab3c6dbfc78844
Diogo Trindade <diogoguarda@netcabo.pt>
2015-10-02 21:11:43 +0100

    - Update CGF - Starting .LSX and Parser

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -1,4 +1,3 @@
-
 function MySceneGraph(filename, scene) {
 	this.loadedOk = null;
 	
@@ -23,17 +22,15 @@ function MySceneGraph(filename, scene) {
  */
 MySceneGraph.prototype.onXMLReady=function() 
 {
-	console.log("XML Loading finished.");
+	console.log("LSX Loading finished.");
 	var rootElement = this.reader.xmlDoc.documentElement;
 	
 	// Here should go the calls for different functions to parse the various blocks
-	var error = this.parseGlobalsExample(rootElement);
-
+	var error = this.parseIllumination(rootElement);
 	if (error != null) {
 		this.onXMLError(error);
 		return;
-	}	
-
+	}
 	this.loadedOk=true;
 	
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
@@ -41,49 +38,38 @@ MySceneGraph.prototype.onXMLReady=function()
 };
 
 
-
-/*
- * Example of method that parses elements of one block and stores information in a specific data structure
- */
-MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
+MySceneGraph.prototype.parseInitials= function(rootElement) {
 	
-	var elems =  rootElement.getElementsByTagName('globals');
+	var elems =  rootElement.getElementsByTagName('INITIALS');
 	if (elems == null) {
-		return "globals element is missing.";
+		return  "INITIALS element is missing.";
 	}
 
 	if (elems.length != 1) {
-		return "either zero or more than one 'globals' element found.";
+		return "either zero or more than one 'INITIALS' element found.";
 	}
-
-	// various examples of different types of access
-	var globals = elems[0];
-	this.background = this.reader.getRGBA(globals, 'background');
-	this.drawmode = this.reader.getItem(globals, 'drawmode', ["fill","line","point"]);
-	this.cullface = this.reader.getItem(globals, 'cullface', ["back","front","none", "frontandback"]);
-	this.cullorder = this.reader.getItem(globals, 'cullorder', ["ccw","cw"]);
-
-	console.log("Globals read from file: {background=" + this.background + ", drawmode=" + this.drawmode + ", cullface=" + this.cullface + ", cullorder=" + this.cullorder + "}");
-
-	var tempList=rootElement.getElementsByTagName('list');
-
-	if (tempList == null  || tempList.length==0) {
-		return "list element is missing.";
-	}
-	
-	this.list=[];
-	// iterate over every element
-	var nnodes=tempList[0].children.length;
-	for (var i=0; i< nnodes; i++)
-	{
-		var e=tempList[0].children[i];
-
-		// process each element and store its information
-		this.list[e.id]=e.attributes.getNamedItem("coords").value;
-		console.log("Read list item id "+ e.id+" with value "+this.list[e.id]);
-	};
 
 };
+
+MySceneGraph.prototype.parseIllumination= function(rootElement) {
+	var elems =  rootElement.getElementsByTagName('ILLUMINATION');
+	if (elems == null) {
+		return  "ILLUMINATION element is missing.";
+	}
+
+	if (elems.length != 1) {
+		return "either zero or more than one 'ILLUMINATION' element found.";
+	}
+
+	var background_tag = elems[0].children[1];
+	this.background = [];
+	this.background[0] = background_tag.attributes.getNamedItem("r").value;
+	this.background[1] = background_tag.attributes.getNamedItem("g").value;
+	this.background[2] = background_tag.attributes.getNamedItem("b").value;
+	this.background[3] = background_tag.attributes.getNamedItem("a").value;
+	console.log("R:"+this.background[1]);
+	console.log("A:"+this.background[3])
+}
 	
 /*
  * Callback to be executed on any read error
--- reader/main.js
+++ reader/main.js
@@ -30,10 +30,11 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
-	var filename=getUrlVars()['file'] || "demo.xml";
+	var filename = getUrlVars()['file'] || "scene.lsx";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
+
 	var myGraph = new MySceneGraph(filename, myScene);
 	
 	// start
